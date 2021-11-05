0c31dbad39cec87fb10ceb0eaab41d29fbb99fe2
Andrei Rybak <rybak.a.v@gmail.com>
2021-11-05 14:02:35 +0100

    add LICENSE.txt

--- bitbucket-pull-request-commit-links.user.js
+++ bitbucket-pull-request-commit-links.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Bitbucket : commit links in PRs
 // @namespace    http://tampermonkey.net/
-// @version      2
+// @version      3
+// @license      MIT
 // @description  Adds convenience links in PRs of Bitbucket v7.6.8
 // @author       Andrei Rybak
 // @match        https://bitbucket.example.com/projects/*/repos/*/pull-requests/*
@@ -10,6 +11,28 @@
 // @homepageURL  https://github.com/rybak/atlassian-tweaks
 // @grant        none
 // ==/UserScript==
+
+/*
+ * Copyright (c) 2021 Andrei Rybak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 (function() {
 	'use strict';
