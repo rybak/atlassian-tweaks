8e3e4927489e07279b2df16c9931ed91ae2a43a7
Sergey Lukashevich <lukashevich@devexperts.com>
2022-11-25 19:35:53 +0100

    jira-project-avatar-as-favicon scripts now detects project icon on project page as well

--- jira-project-avatar-as-favicon.user.js
+++ jira-project-avatar-as-favicon.user.js
@@ -6,6 +6,7 @@
 // @description  Changes browser tab icon to JIRA project icon
 // @author       Sergey Lukashevich
 // @match        https://jira.example.com/browse/*
+// @match        https://jira.example.com/projects/*
 // @icon         https://jira.atlassian.com/favicon.ico
 // @homepageURL  https://github.com/rybak/atlassian-tweaks
 // @grant        none
@@ -15,6 +16,15 @@
     'use strict';
 
     let projectAvatar = document.getElementById('project-avatar');
+    if (!projectAvatar) {
+        let elements = document.getElementsByClassName('aui-avatar-project');
+        if (elements.length === 1) {
+            let byTagName = elements[0].getElementsByTagName('img');
+            if (byTagName.length === 1) {
+                projectAvatar = byTagName[0];
+            }
+        }
+    }
     let shortcutIco = document.querySelector('link[rel="shortcut icon"]');
     if (projectAvatar && shortcutIco) {
         shortcutIco.href = projectAvatar.src;
