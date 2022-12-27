0e1051467aa247970527e771d59ac12585e556ff
Andrei Rybak <rybak.a.v@gmail.com>
2022-12-27 17:50:20 +0100

    jira-project-avatar-as-favicon: clean up
    Bring recently added jira-project-avatar-as-favicon.user.js closer to
    the conventions of other scripts:
    
      - Spell "Jira" without ALL CAPS
      - Add in-code license
      - Re-format the JS code with tabs as indentation

--- jira-project-avatar-as-favicon.user.js
+++ jira-project-avatar-as-favicon.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Jira: Project icon as tab icon
 // @namespace    http://tampermonkey.net/
-// @version      2
+// @version      3
 // @license      MIT
-// @description  Changes browser tab icon to JIRA project icon
+// @description  Changes browser tab icon to Jira project icon
 // @author       Sergey Lukashevich
 // @match        https://jira.example.com/*
 // @icon         https://jira.atlassian.com/favicon.ico
@@ -11,21 +11,43 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+/*
+ * Copyright (c) 2022 Sergey Lukashevich
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-    let projectAvatar = document.getElementById('project-avatar');
-    if (!projectAvatar) {
-        let elements = document.getElementsByClassName('aui-avatar-project');
-        if (elements.length === 1) {
-            let byTagName = elements[0].getElementsByTagName('img');
-            if (byTagName.length === 1) {
-                projectAvatar = byTagName[0];
-            }
-        }
-    }
-    let shortcutIco = document.querySelector('link[rel="shortcut icon"]');
-    if (projectAvatar && shortcutIco) {
-        shortcutIco.href = projectAvatar.src;
-    }
+(function() {
+	'use strict';
+
+	let projectAvatar = document.getElementById('project-avatar');
+	if (!projectAvatar) {
+		let elements = document.getElementsByClassName('aui-avatar-project');
+		if (elements.length === 1) {
+			let byTagName = elements[0].getElementsByTagName('img');
+			if (byTagName.length === 1) {
+				projectAvatar = byTagName[0];
+			}
+		}
+	}
+	let shortcutIco = document.querySelector('link[rel="shortcut icon"]');
+	if (projectAvatar && shortcutIco) {
+		shortcutIco.href = projectAvatar.src;
+	}
 })();
